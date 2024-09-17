48438e5a498a4e0973ea9db3bbb016d1bcb4d591
MD JAHIDUL ISLAM JIHAD <mdj847621@gmail.com>
2024-09-17 20:06:13 +0600

    update form part

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ function connectName(){
     const myfunction1 = document.getElementById('text1').value;
     const myfunction2 = document.getElementById('text2').value;
     const myfunction3 = document.getElementById('text3').value;
+    const myfunction4 = document.getElementById('text4').value;
+    const myfunction5 = document.getElementById('text5').value;
+    const myfunction6 = document.getElementById('text6').value;
+    const myfunction7 = document.getElementById('text7').value;
 
     const containsNumber = /\d/;
 
@@ -15,5 +19,7 @@ function connectName(){
         return;
     }
 
-    document.getElementById('result').innerText = 'Successfull Your All Information '
+    const sum = myfunction1 + ' ' + myfunction2 + ' ' + myfunction3 + ' ' + myfunction4 + ' ' + myfunction5 + ' ' + myfunction6 + ' ' + myfunction7;
+
+    document.getElementById('result').innerText = 'User fullname is ' + sum
 }
